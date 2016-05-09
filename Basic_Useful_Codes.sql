31c46a5c17d01d41091c11c35fa83a8a0e733e61
stonephoenix <yangjian.sir@gmail.com>
2016-05-09 18:02:30 -0500

    Useful Codes - Basics

---
+++ Basic_Useful_Codes.sql
@@ -0,0 +1,11 @@
+
+
+
+-- Nth largest
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  set N = N-1;
+  RETURN (
+      select distinct Salary from Employee order by Salary desc limit N, 1 
+  );
+END
